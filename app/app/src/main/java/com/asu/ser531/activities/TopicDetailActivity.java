261987fbe98f299e1788b7466fd35e18fa98a5d5
Rohit <r4rohit002@gmail.com>
2019-11-14 13:51:04 -0700

    Added Topic detail activty

--- app/app/src/main/java/com/asu/ser531/activities/TopicDetailActivity.java
+++ app/app/src/main/java/com/asu/ser531/activities/TopicDetailActivity.java
@@ -2,29 +2,28 @@ package com.asu.ser531.activities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.asu.ser531.R;
 import com.asu.ser531.model.Topic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SubTopicListActivity extends AppCompatActivity {
+public class TopicDetailActivity extends AppCompatActivity {
 
+    private static final String TAG = "TopicDetailActivity";
 
-    private static final String TAG = "SubTopicListActivity";
-    
+    private TextView textView;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_topic);
-
+        setContentView(R.layout.activity_topic_detail);
+        textView = findViewById(R.id.textName);
 
         Topic topic = (Topic) getIntent().getSerializableExtra("Topic");
 
         Log.d(TAG, "onCreate: "+topic);
 
-
     }
-
-
 }
