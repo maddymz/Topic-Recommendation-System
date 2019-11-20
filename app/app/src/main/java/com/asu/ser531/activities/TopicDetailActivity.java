b988449042e7e1b5c0b864f274cab53c5e03797f
Rohit <r4rohit002@gmail.com>
2019-11-20 00:47:29 -0700

    Integrated YouTube

--- app/app/src/main/java/com/asu/ser531/activities/TopicDetailActivity.java
+++ app/app/src/main/java/com/asu/ser531/activities/TopicDetailActivity.java
@@ -1,24 +1,34 @@
 package com.asu.ser531.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.asu.ser531.Config;
 import com.asu.ser531.ItemClickListener;
 import com.asu.ser531.R;
 import com.asu.ser531.adapters.TopicAdapter;
 import com.asu.ser531.model.Topic;
 import com.asu.ser531.utilities.AppUtility;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TopicDetailActivity extends AppCompatActivity implements ItemClickListener {
+public class TopicDetailActivity extends YouTubeBaseActivity implements ItemClickListener, YouTubePlayer.OnInitializedListener {
 
     private static final String TAG = "TopicDetailActivity";
     private TextView textView;
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+
 
 //    private RecyclerView prereqRv;
 //    private TopicAdapter adapter;
@@ -34,6 +44,10 @@ public class TopicDetailActivity extends AppCompatActivity implements ItemClickL
 //        getDummyTopic();
         getTopicFromIntent();
         textView = findViewById(R.id.prereqText);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+
+
 ////        prereqRv = findViewById(R.id.prereqRV);
 //        llm = new LinearLayoutManager(this);
 //        adapter = new TopicAdapter(this, topic.getPrereqs());
@@ -54,5 +68,34 @@ public class TopicDetailActivity extends AppCompatActivity implements ItemClickL
     @Override
     public void itemClicked(Object object) {
 
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = "Error";
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
